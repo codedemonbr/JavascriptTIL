a66ad642a5e84c89461c6edb9a188b5443998217
Henrique <henrique.thsantos.ths@gmail.com>
2021-06-01 18:16:02 -0300

    Promises #1

--- esnext/promises.js
+++ esnext/promises.js
@@ -6,8 +6,7 @@ function falarDepoisDe(segundo, frase) {
     });
 }
 
-falarDepoisDe(10, 'Que legal!')
-    .then((frase) => frase.concat('?!?'))
+falarDepoisDe(10, "Que legal!")
+    .then((frase) => frase.concat("?!?"))
     .then((outraFrase) => console.log(outraFrase))
     .catch((e) => console.log(e));
-x;
